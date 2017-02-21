93ea1fe1fd0dfac6b8a047274d3d0bb1ae590556
panchaozhi <1099958338@qq,com>
2017-02-21 13:53:05 +0800

    DetailView组件的使用

--- backend/views/post/view.php
+++ backend/views/post/view.php
@@ -7,7 +7,7 @@ use yii\widgets\DetailView;
 /* @var $model common\models\Post */
 
 $this->title = $model->title;
-$this->params['breadcrumbs'][] = ['label' => 'Posts', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => '文章列表', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="post-view">
@@ -15,8 +15,8 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('更新', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('删除', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
@@ -33,21 +33,58 @@ $this->params['breadcrumbs'][] = $this->title;
             'label_img',
             'summary',
             'content:ntext',
-            'cate_id',
+            //'cate_id',
+            //可以这么用
+            [
+                'attribute' => 'cate_id',
+                'value' => $model->cate->name,
+            ],
+            //也可以这么用
+            //'cate.name',
             'user_id',
             'user_name',
-            'is_valid',
-            'browser',
-            'created_at',
-            'updated_at',
+            //'is_valid',
+            [
+                'label' => '文章状态',
+                'attribute' => 'is_valid',
+                'value' => function ($model) {
+                    return $model->is_valid == 1 ? "已发布" : "未发布";
+                },
+                'filter' => ['0' => '未发布', '1' => '已发布'],
+            ],
+            'browser'=>[
+                    'attribute'=>'browser',
+                    'value'=>$model->browser.'次',
+            ],
+            //'created_at',
+            [
+                'label' => '创建时间',
+                'attribute' => 'created_at',
+                'value' => date("Y-m-d H:i:s"),
+            ],
+            //'updated_at',
+            [
+                'label' => '创建时间',
+                'attribute' => 'updated_at',
+                'value' => date("Y-m-d H:i:s"),
+            ],
             'zan',
             'cai',
             'ip_address',
             'keywords',
-            'last_comment',
+            //'last_comment',
+            [
+                'label' => '创建时间',
+                'attribute' => 'last_comment',
+                'value' => date("Y-m-d H:i:s"),
+            ],
             'signature',
             'nickname',
         ],
+        'options' => [
+            'class' => 'table table-striped table-bordered detail-view'
+        ],
+        'template' => '<tr><th style="width: 120px">{label}</th><td>{value}</td></tr>',
     ]) ?>
 
 </div>
